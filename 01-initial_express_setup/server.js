5f7c58501d851ac6c34e9f0ffcd8831a80f4fa83
Juan Almonte <almonteestrella@gmail.com>
2023-08-11 10:53:10 -0400

    params initial setup

--- 01-initial_express_setup/server.js
+++ 01-initial_express_setup/server.js
@@ -4,7 +4,15 @@ const app = express();
 const { products } = require('./data');
 
 app.get('/', (req, res) => {
-    res.json(products);
+    res.send('<h1>home page</h1><a href="/api/products">produts</a>');
+});
+
+app.get('/api/products', (req, res) => {
+    const newProductList = products.map(({ id, name, price, image }) => {
+        return { id, name, price, image };
+    });
+
+    res.json(newProductList);
 });
 
 app.listen(5000, () => console.log('server is listening on port 5000'));
