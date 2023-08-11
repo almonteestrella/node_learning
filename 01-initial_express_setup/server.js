9422b3c3ddb12dd976b612c98e5bd1948bae3113
Juan Almonte <almonteestrella@gmail.com>
2023-08-11 10:28:52 -0400

    example using res.json

--- 01-initial_express_setup/server.js
+++ 01-initial_express_setup/server.js
@@ -1,30 +1,10 @@
 const express = require('express');
 const app = express();
 
-//setup static and middleware
-app.use(express.static('./public'));
+const { products } = require('./data');
 
-//home page
 app.get('/', (req, res) => {
-    res.status(200).send('home page');
-});
-
-//about page
-app.get('/about', (req, res) => {
-    res.status(200).send('about page');
-});
-
-//404
-app.all('*', (req, res) => {
-    res.status(404).send('page not found');
+    res.json(products);
 });
 
 app.listen(5000, () => console.log('server is listening on port 5000'));
-
-//most used methods
-// app.get;
-// app.post;
-// app.put;
-// app.patch;
-// app.delete;
-// app.all
