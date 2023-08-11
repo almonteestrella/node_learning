81c2c348b5d7954e4d59db79360570a7fe1f8d4c
Juan Almonte <almonteestrella@gmail.com>
2023-08-11 12:45:25 -0400

    query string intro

--- 01-initial_express_setup/server.js
+++ 01-initial_express_setup/server.js
@@ -29,5 +29,25 @@ app.get('/api/products/:productID', (req, res) => {
 
     res.json(singleProduct);
 });
+app.get('/api/query', (req, res) => {
+    const { search, limit } = req.query;
+    let sortedProducts = [...products];
+
+    if (search) {
+        sortedProducts = sortedProducts.filter((product) =>
+            product.name.startsWith(search)
+        );
+    }
+
+    if (limit) {
+        sortedProducts = sortedProducts.slice(0, Number(limit));
+    }
+
+    if (sortedProducts.length < 1) {
+        res.status(200).json({ success: true, data: [] });
+    }
+
+    res.status(200).json(sortedProducts);
+});
 
 app.listen(5000, () => console.log('server is listening on port 5000'));
