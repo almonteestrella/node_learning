5d84c3cbb23c00ff4bd2f6bc6f94233b18202931
Juan Almonte <almonteestrella@gmail.com>
2023-08-14 12:45:20 -0400

    post request example

--- 01-initial_express_setup/server.js
+++ 01-initial_express_setup/server.js
@@ -1,24 +1,39 @@
 const express = require('express');
-const logger = require('./middleware/logger');
-const authorized = require('./middleware/authorized');
-
-//third party dependency
-const morgan = require('morgan');
+let { people } = require('./data');
 
 const app = express();
 
-//middleware
-// req => middleware => res
+//static assets
+app.use(express.static('./methods-public'));
 
-// app.use([logger, authorized]); //apply the middleware functions in ALL my routes and should be on top of all the routes
+//parse form data
+app.use(express.urlencoded({ extended: false })); // go to definition
 
-app.use(morgan('tiny')); //logger functionality
+//parse json
+app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('home page');
+app.get('/api/people', (req, res) => {
+    res.status(200).send({ success: true, data: people });
 });
-app.get('/about', (req, res) => {
-    res.send('about page');
+
+app.post('/api/people', (req, res) => {
+    const { name } = req.body;
+    if (!name) {
+        res.status(400).json({
+            success: false,
+            msg: 'please provide name value',
+        });
+    }
+    res.status(201).json({ success: true, person: name });
+});
+
+app.post('/login', (req, res) => {
+    const { name } = req.body;
+
+    if (name) {
+        res.status(200).send(`hello and welcome ${name}`);
+    }
+    res.status(401).send('please, provide credentials');
 });
 
 app.listen(5000, () => console.log('server is listening on port 5000'));
