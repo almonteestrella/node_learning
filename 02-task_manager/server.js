76a6a8ba1040035fe86e9ce5cea2da0436e2140e
almonteestrella <49617330+almonteestrella@users.noreply.github.com>
2023-08-18 15:00:22 -0400

    Updated server.js

--- 02-task_manager/server.js
+++ 02-task_manager/server.js
@@ -15,4 +15,4 @@ app.use('/api/v1/tasks', tasks);
 const port = 5000;
 app.listen(port, console.log(`server listen on port: ${port}...`));
 
-// DATABASE_URL=mongodb+srv://first_mongoDB:developer1411809@nodeexpressprojects.5z0kyqv.mongodb.net/first_testingDB?retryWrites=true&w=majority
+
