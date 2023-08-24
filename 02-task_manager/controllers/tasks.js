26db3cbf84631546da467af9fba9f652a9041ef6
Juan Almonte <almonteestrella@gmail.com>
2023-08-24 14:44:11 -0400

    Custom error handler implementation

--- 02-task_manager/controllers/tasks.js
+++ 02-task_manager/controllers/tasks.js
@@ -1,3 +1,4 @@
+const { createCustomError } = require('../errors/custom-error');
 const asyncWrapper = require('../middlewares/async');
 const Task = require('../models/Task');
 
@@ -10,11 +11,11 @@ const createTask = asyncWrapper(async (req, res) => {
     const task = await Task.create(req.body);
     res.status(201).json({ task });
 });
-const getSingleTask = asyncWrapper(async (req, res) => {
+const getSingleTask = asyncWrapper(async (req, res, next) => {
     const { id: taskID } = req.params;
     const task = await Task.findOne({ _id: taskID });
     if (!task) {
-        return res.status(404).json({ msg: `No task with the id: ${taskID}` });
+        return next(createCustomError(`No task with the id: ${taskID}`, 404));
     }
     res.status(200).json({ task });
 });
@@ -25,7 +26,7 @@ const updateTask = asyncWrapper(async (req, res) => {
         runValidators: true,
     });
     if (!task) {
-        return res.status(404).json({ msg: `No task with the id: ${taskID}` });
+        return next(createCustomError(`No task with the id: ${taskID}`, 404));
     }
     res.status(200).json({ task });
 });
@@ -33,7 +34,7 @@ const deleteTask = asyncWrapper(async (req, res) => {
     const { id: taskID } = req.params;
     const task = await Task.findOneAndDelete({ _id: taskID });
     if (!task) {
-        return res.status(404).json({ msg: `No task with the id: ${taskID}` });
+        return next(createCustomError(`No task with the id: ${taskID}`, 404));
     }
     res.status(200).json({ task });
 });
--- 02-task_manager/server.js
+++ 02-task_manager/server.js
@@ -4,16 +4,18 @@ const app = express();
 const tasks = require('./routes/tasks');
 require('dotenv').config();
 const notFound = require('./middlewares/notFound');
+const errorHandlerMiddleware = require('./middlewares/error-handler');
 
 //middleware
-app.use(express.json());
 app.use(express.static('./public'));
-app.use(notFound);
+app.use(express.json());
 
 //routes
 app.use('/api/v1/tasks', tasks);
+app.use(notFound);
+app.use(errorHandlerMiddleware);
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 const start = async () => {
     try {
         await dbConnection(process.env.DATABASE_URL);
